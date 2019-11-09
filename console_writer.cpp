42af208df4264e16612af92a0fe9e3e0cd2b6ccd
pavel204812@gmail.com <2606QWmg>
2019-11-09 22:21:40 +0200

    add fumction and test

--- console_writer.cpp
+++ console_writer.cpp
@@ -4,13 +4,10 @@
 
 using namespace std;
 
-int main()
-{
+const int SIZE = 200;
+const int LSIZE = 100000;
 
-	cout << "Enter file name: ";
-	char file_name[100];
-	cin.getline(file_name, 100);
-
+void CreateFile(char file_name[], int SIZE ) {
 	ifstream open_file;
 
 	string directori = "C:\\Users\\pavel\\Desktop\\";
@@ -19,21 +16,39 @@ int main()
 	const char* CCdirectori = directori.c_str();
 
 	open_file.open(CCdirectori, ios::app);//open correct directori from file
+}
 
-	cout << "Enter text from file: ";
-	char input_file[1000];
-	cin.getline(input_file, 100);
+
+void WriteFile(char input_file[], int LSIZE, char file_name[], int SIZE ) {
+	cin.getline(input_file, LSIZE);
 
 	ofstream file(file_name);
 
-	if (!file.is_open()) 
+	if (!file.is_open())
 		cout << "not open file!\n";
-	
+
 	else
 	{
 		file << input_file; // write from file
 		cout << input_file << endl; // print 
 	}
+
+	file.close();
+}
+
+int main ()
+{
+
+	cout << "Enter file name: ";
+	char file_name[SIZE];
+	cin.getline(file_name, SIZE);
+
+	CreateFile(file_name, SIZE);
+	
+	cout << "Enter text from file: ";
+	char input_file[LSIZE];
+
+	WriteFile(input_file, LSIZE, file_name, SIZE);
 
 	return 0;
 }
